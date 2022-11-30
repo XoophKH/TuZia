de74ed574ba007d9a66a7ae3fd37d2281a8bc180
XoophKH <119462764+XoophKH@users.noreply.github.com>
2022-12-01 00:14:53 +0100

    Add files via upload

---
+++ Net/Controllers/FroggerController.cs
@@ -0,0 +1,29 @@
+﻿using esame.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace esame.Controllers
+{
+    public class FroggerController : Controller
+    {
+        private static List<FroggerViewModel> frogger = new List<FroggerViewModel>();
+
+        public IActionResult Index()
+        {
+            FroggerViewModel.Ordina(frogger);
+            return View(frogger);
+        }
+        public IActionResult Inserct()
+        {
+            var frogVm = new FroggerViewModel();
+            return View(frogVm);
+        }
+
+        public IActionResult InserctFrogger(FroggerViewModel froggerViewModel)
+        {
+            frogger.Add(froggerViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Net/Models/FroggerViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace esame.Models
+{
+    public class FroggerViewModel
+    {
+        public int Punteggio  { get; set; }
+
+        public static void Ordina(List<FroggerViewModel> list)
+        {
+           for(int i = 0; i < list.Count; i++)
+            {
+                for(int j = 0; j < list.Count; j++)
+                {
+                    if(list[i].Punteggio > list[j].Punteggio)
+                    {
+                        int temp = list[i].Punteggio;
+                        list[i].Punteggio = list[j].Punteggio;
+                        list[j].Punteggio = temp;
+                    }
+                }
+            }
+        }
+
+       
+    }
+}
